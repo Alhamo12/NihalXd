49764f81186af959c93bc09be0bd1ebe433bfb5e
ROHIT OP <104740062+SELENEBOTSTG@users.noreply.github.com>
2022-10-06 18:18:14 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SELENEBOTSTG/NihalMusic",
+    "https://telegra.ph/file/f84d28d91512a445ecce1.mp4c",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
